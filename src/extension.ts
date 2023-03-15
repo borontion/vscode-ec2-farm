e3a0af0b608a1b2a98bc499f9dc99e1c003c62af
Pengzhan Zhao <borontion@gmail.com>
2023-03-14 23:19:49 -0700

    Configure AWS account

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,31 @@
 import * as vscode from 'vscode';
 import { EC2InstanceListViewProvider } from './view';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.registerTreeDataProvider('ec2-farm-instance-list', new EC2InstanceListViewProvider());
+	const ec2InstanceListViewProvider = new EC2InstanceListViewProvider(context);
+
+	vscode.window.registerTreeDataProvider('ec2-farm-instance-list',
+		ec2InstanceListViewProvider);
+
+	// Configure account
+	vscode.commands.registerCommand('ec2-farm.configureAccount', () => {
+		ec2InstanceListViewProvider.configureAccount();
+	});
+
+	// Describe account
+	vscode.commands.registerCommand('ec2-farm.describeAccount', () => {
+		ec2InstanceListViewProvider.describeAccount();
+	});
+
+	// Cleanup account
+	vscode.commands.registerCommand('ec2-farm.cleanupAccount', () => {
+		ec2InstanceListViewProvider.cleanupAccount();
+	});
+
+	// Refresh
+	vscode.commands.registerCommand('ec2-farm.refresh', () => {
+		ec2InstanceListViewProvider.refresh();
+	});
 }
 
 // This method is called when your extension is deactivated
--- src/view.ts
+++ src/view.ts
@@ -9,12 +9,86 @@ class EC2Instance extends vscode.TreeItem {
   }
 }
 
+class Account {
+  awsAccessKeyId: string | undefined;
+  awsSecretAccessKey: string | undefined;
+  region: string | undefined;
+}
+
 export class EC2InstanceListViewProvider implements vscode.TreeDataProvider<EC2Instance> {
+  context: vscode.ExtensionContext;
+  account: Account | undefined;
+
+  private _onDidChangeTreeData: vscode.EventEmitter<EC2Instance | undefined> = new vscode.EventEmitter<EC2Instance | undefined>();
+  readonly onDidChangeTreeData: vscode.Event<EC2Instance | undefined> = this._onDidChangeTreeData.event;
+
+  constructor(context: vscode.ExtensionContext) {
+    this.context = context;
+  }
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire(undefined);
+  }
+
+  configureAccount(): void {
+    const account = new Account();
+
+    // Input AWS configuration
+    vscode.window.showInputBox({
+      prompt: 'AWS Access Key ID',
+      placeHolder: 'AWS Access Key ID',
+    }).then((awsAccessKeyId) => {
+      if (awsAccessKeyId) {
+        account.awsAccessKeyId = awsAccessKeyId;
+      }
+
+      vscode.window.showInputBox({
+        prompt: 'AWS Secret Access Key',
+        placeHolder: 'AWS Secret Access Key',
+      }).then((awsSecretAccessKey) => {
+        if (awsSecretAccessKey) {
+          account.awsSecretAccessKey = awsSecretAccessKey;
+        }
+
+        vscode.window.showInputBox({
+          prompt: 'Region',
+          placeHolder: 'Region',
+        }).then((region) => {
+          if (region) {
+            account.region = region;
+          }
+
+          this.account = account;
+          this.refresh();
+        });
+      });
+    });
+  }
+
+  describeAccount(): void {
+    if (this.account) {
+      vscode.window.showInformationMessage(
+        `AWS Access Key ID: ${this.account.awsAccessKeyId}\n
+         AWS Secret Access Key: ${this.account.awsSecretAccessKey}\n
+         Region: ${this.account.region}`);
+    } else {
+      vscode.window.showInformationMessage('Account is not configured');
+    }
+  }
+
+  cleanupAccount(): void {
+    this.account = undefined;
+  }
+
   getTreeItem(element: EC2Instance): vscode.TreeItem {
     return element;
   }
 
   getChildren(element?: EC2Instance): Thenable<EC2Instance[]> {
+    if (!this.account) {
+      return Promise.resolve([]);
+    }
+
     if (element) {
       return Promise.resolve([
         new EC2Instance('instance1-1', vscode.TreeItemCollapsibleState.None),
